d5b654e3b1e1013c705895da2432198d2d2bf736
Agust0124 <105009985+Agust0124@users.noreply.github.com>
2023-12-05 13:58:20 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.register('./sw.js')
+    .then(registrado => console.log("El service worker se registro",
+    registrado))
+    .catch(error=>console.log("Error al registrar el servicio worker",
+    error));
+
+} else {
+    console.log("el servicio worker no es compatible");
+
+
+}
+
